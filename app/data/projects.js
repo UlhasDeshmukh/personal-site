1a8e290e5600564f40c2cd29a4afb2920ea31489
Ulhas Deshmukh <ulhas.deshmukh@gmail.com>
2019-04-11 21:47:48 +0530

    image changes

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,7 +4,7 @@ const data = [
     title: 'Mobile Device Farm',
     subtitle: 'SmartPhone Test Farm based on openstf',
     link: '/projects/MobileFarm/',
-    image: 'images/projects/STF.png',
+    image: 'images/projects/STF.jpeg',
     date: '2016-12-15',
     desc: 'Control and manage real Smartphone devices from your browser'
       + ' based on openstf.io framework,'
